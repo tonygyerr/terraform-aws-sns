006385e4011056fd101f5b4f72ada5f817f1bd37
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-09-10 13:16:50 -0400

    kms

--- kms.tf
+++ kms.tf
@@ -1,10 +1,10 @@
-# resource "aws_kms_key" "sns" {
-#   description             = "rds master key for ${var.name}-${var.tags["vpc"]}"
-#   deletion_window_in_days = 30
-#   enable_key_rotation     = "true"
-# }
+resource "aws_kms_key" "sns" {
+  description             = "rds master key for ${var.name}-${var.tags["vpc"]}"
+  deletion_window_in_days = 30
+  enable_key_rotation     = "true"
+}
 
-# resource "aws_kms_alias" "sns" {
-#   name          = "alias/${var.name}-${var.tags["vpc"]}-rds-key"
-#   target_key_id = "${aws_kms_key.sns.key_id}"
-# }
+resource "aws_kms_alias" "sns" {
+  name          = "alias/${var.name}-${var.tags["vpc"]}-rds-key"
+  target_key_id = "${aws_kms_key.sns.key_id}"
+}
