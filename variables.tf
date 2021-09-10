59a986905786e4787a30dfe06db841d731b77795
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-09-10 13:11:59 -0400

    variables.tf

--- variables.tf
+++ variables.tf
@@ -7,109 +7,109 @@ variable "create_sns_topic" {
 variable "name" {
   description = "The name of the SNS topic to create"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "name_prefix" {
   description = "The prefix name of the SNS topic to create"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "display_name" {
   description = "The display name for the SNS topic"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "policy" {
   description = "The fully-formed AWS policy as JSON"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "delivery_policy" {
   description = "The SNS delivery policy"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "application_success_feedback_role_arn" {
   description = "The IAM role permitted to receive success feedback for this topic"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "application_success_feedback_sample_rate" {
   description = "Percentage of success to sample"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "application_failure_feedback_role_arn" {
   description = "IAM role for failure feedback"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "http_success_feedback_role_arn" {
   description = "The IAM role permitted to receive success feedback for this topic"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "http_success_feedback_sample_rate" {
   description = "Percentage of success to sample"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "http_failure_feedback_role_arn" {
   description = "IAM role for failure feedback"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "lambda_success_feedback_role_arn" {
   description = "The IAM role permitted to receive success feedback for this topic"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "lambda_success_feedback_sample_rate" {
   description = "Percentage of success to sample"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "lambda_failure_feedback_role_arn" {
   description = "IAM role for failure feedback"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "sqs_success_feedback_role_arn" {
   description = "The IAM role permitted to receive success feedback for this topic"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "sqs_success_feedback_sample_rate" {
   description = "Percentage of success to sample"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "sqs_failure_feedback_role_arn" {
   description = "IAM role for failure feedback"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "kms_master_key_id" {
   description = "The ID of an AWS-managed customer master key (CMK) for Amazon SNS or a custom CMK"
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "tags" {
